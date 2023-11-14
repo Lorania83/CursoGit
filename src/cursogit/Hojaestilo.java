fd6c29d91bf160a45a17864f388cf17a72b4b5a1
Tania <tanialorences@gmail.com>
2023-11-14 17:09:23 +0100

    Párrafo y tamaño de fuente

--- src/cursogit/Hojaestilo.java
+++ src/cursogit/Hojaestilo.java
@@ -4,7 +4,8 @@ public class Hojaestilo {
 
 	public static void main(String[] args) {
 		System.out.println("Estilos");
-
+		System.out.println("Añadir enlace");
+		System.out.println("Proyecto empresarial");
 	}
 
 }
